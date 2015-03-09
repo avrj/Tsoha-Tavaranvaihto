528cd3b7cf1679673d5642017b4e9accf5d51a3f
avrj <anraiha@helsinki.fi>
2015-03-09 22:43:56 +0200

    test

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,15 +2,11 @@
  
 # --- !Ups
  
-CREATE TABLE Users (
-    id bigint(20) NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    fullname varchar(255) NOT NULL,
-    isAdmin boolean NOT NULL,
-    PRIMARY KEY (id)
-);
- 
+CREATE TABLE User (
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  password varchar(50) NOT NULL
+
 # --- !Downs
  
 DROP TABLE Users;
