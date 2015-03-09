3efb3091d5751a06f1296b5c865b2d8df059f715
avrj <anraiha@helsinki.fi>
2015-03-09 22:01:36 +0200

    created test model

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,55 @@
 package controllers;
 
 import play.*;
+import play.db.DB;
 import play.mvc.*;
 
 import views.html.*;
 
+import models.*;
+
+import java.sql.*;
+
 public class Application extends Controller {
+    private static Connection connection = DB.getConnection();
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        System.out.println("Opened database successfully");
+
+        PreparedStatement kysely = null;
+        ResultSet tulokset = null;
+        int tulos = 0;
+
+        try {
+            //Alustetaan muuttuja jossa on Select-kysely, joka palauttaa lukuarvon:
+            String sqlkysely = "SELECT COUNT(*) FROM User as two";
+
+            kysely = connection.prepareStatement(sqlkysely);
+            tulokset = kysely.executeQuery();
+            if(tulokset.next()) {
+                //Tuloksen arvoksi pitäisi tulla numero kaksi.
+                tulos = tulokset.getInt("two");
+            } else {
+                //out.println("Virhe!");
+            }
+        } catch (Exception e) {
+            //out.println("Virhe: " + e.getMessage()");
+        }
+
+        try {
+            tulokset.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            kysely.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Testi testi = new Testi();
+        testi.setTesti(5);
+
+        return ok(index.render(testi.getTesti() + "Your new application is ready." + tulos));
     }
 
 }
---
+++ app/models/Testi.java
@@ -0,0 +1,20 @@
+package models;
+
+/**
+ * Created by avrj on 9.3.2015.
+ */
+public class Testi {
+    private int testi;
+
+    public Testi() {
+        testi = 0;
+    }
+
+    public int getTesti() {
+        return testi;
+    }
+
+    public void setTesti(int testi) {
+        testi = this.testi;
+    }
+}
